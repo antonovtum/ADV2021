bbd93c2af232c950b76918e3e4eb35347a10fc00
antonovtum <55877072+antonovtum@users.noreply.github.com>
2021-03-26 20:24:37 +0100

    sm-testy

---
+++ Assets/AnimManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimManager : MonoBehaviour
+{
+    public Animator animator;
+    public string boolParam;
+    bool b = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "trigger") {
+            //activate close animation
+            Debug.Log("Collision detected");
+            b = !b;
+            animator.SetBool(boolParam, b);
+        }
+    }
+}
