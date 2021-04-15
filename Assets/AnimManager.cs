513d2a7d15f2b709c6e79a400fac84ce04ba9afc
antonovtum <55877072+antonovtum@users.noreply.github.com>
2021-04-15 23:32:31 +0200

    state machines

--- Assets/AnimManager.cs
+++ Assets/AnimManager.cs
@@ -6,11 +6,17 @@ public class AnimManager : MonoBehaviour
 {
     public Animator animator;
     public string boolParam;
+    public float timeClosed;
     bool b = true;
+
+    public AudioClip swaneeUp;
+    public AudioClip swaneeDown;
+
+    private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent(typeof(AudioSource)) as AudioSource;
     }
 
     // Update is called once per frame
@@ -24,8 +30,33 @@ public class AnimManager : MonoBehaviour
         if (other.tag == "trigger") {
             //activate close animation
             Debug.Log("Collision detected");
-            b = !b;
-            animator.SetBool(boolParam, b);
+            if (b)
+            {
+                //previously was opened, time to close
+                b = false;
+                animator.SetBool(boolParam, b);
+
+                //play closing clip
+                audioSource.clip = swaneeDown;
+                audioSource.pitch = 1.9f;
+                audioSource.Play();
+                //start timer to open again
+                StartCoroutine(openTimeOut());
+
+            }
         }
+    }
+
+    IEnumerator openTimeOut()
+    {
+        Debug.Log("started coroutine");
+        yield return new WaitForSeconds(timeClosed);
+        b = true;
+        animator.SetBool(boolParam, b);
+        //play opening clip
+        audioSource.clip = swaneeUp;
+        audioSource.pitch = 1;
+        audioSource.Play();
+        Debug.Log("ended coroutine");
     }
 }
