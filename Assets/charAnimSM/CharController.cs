6bbfcafc2a024ee5265fb0c3864b570ec7d551f9
antonovtum <55877072+antonovtum@users.noreply.github.com>
2021-04-25 23:16:02 +0200

    char anim with SM

---
+++ Assets/charAnimSM/CharController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    private Animator animator;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.I))
+        {
+            //forward
+            animator.SetFloat("vertical", Mathf.Lerp(animator.GetFloat("vertical"), 1, speed * Time.deltaTime));
+        } else if (Input.GetKey(KeyCode.K))
+        {
+            //backwards
+            animator.SetFloat("vertical", Mathf.Lerp(animator.GetFloat("vertical"), -1, speed * Time.deltaTime));
+        }
+        else
+        {
+            //stop
+            animator.SetFloat("vertical", Mathf.Lerp(animator.GetFloat("vertical"), 0, speed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            //left
+            animator.SetFloat("horizontal", Mathf.Lerp(animator.GetFloat("horizontal"), -1, speed * Time.deltaTime));
+        } else if (Input.GetKey(KeyCode.L))
+        {
+            //right
+            animator.SetFloat("horizontal", Mathf.Lerp(animator.GetFloat("horizontal"), 1, speed * Time.deltaTime));
+        }
+        else
+        {
+            //stop
+            animator.SetFloat("horizontal", Mathf.Lerp(animator.GetFloat("horizontal"), 0, speed * Time.deltaTime));
+        }
+    }
+}
